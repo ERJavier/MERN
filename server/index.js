fc8f5c17901a9caf1ce671c7783e08130458de11
Javier Cepeda <erjavierc@gmail.com>
2023-02-10 22:28:23 -0500

    added authentification, signing

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path  from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js"
 import { register } from "./controllers/auth.js"
 
 
@@ -44,6 +45,10 @@ const upload = multer({ storage });
 // ROUTES WITH FILES
 
 app.post("/auth/register", upload.single("picture"), register);
+
+
+//Routes 
+app.use("/auth", authRoutes);
 
 
 // MONGOOSE SET-UP
