10c12ce002ec734c610c02dce875c0257156df1d
Javier Cepeda <erjavierc@gmail.com>
2023-02-11 13:41:45 -0500

    post functionalities done

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,80 @@
-import Posts from "../models/Posts.js"
+import Post from "../models/Posts.js"
+import User from "../models/User.js";
+
+// CREATE 
+
+export const createPost = async (req, res) => {
+    try {
+        const { userId, description, picturePath } = req.body;
+        const user = await User.findById(userId);
+        const newPost = new Post({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            description,
+            userPicturePath: user.PicturePath,
+            picturePath,
+            likes: {},
+            comments: []
+
+        })
+        await newPost.save();
+
+        const post = await Post.find();
+
+        res.status(201).json(post)
+
+    } catch (err) {
+        res.status(409).json({ messega: err.messega});
+    }
+}
+
+// READ 
+
+export const getFeedPost = async (req, res) => {
+    try {
+        const post = await Post.find();
+        res.status(201).json(post);
+        
+    } catch (err) {
+        res.status(404).json({ messega: err.messega });
+    }
+}
+
+export const getUserPosts = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const post = await Post.find({ userId});
+        res.status(201).json(post);
+    } catch (err) {
+        res.status(404).json({ messega: err.messega });
+    }
+}
+
+// UPDATE 
+
+export const likePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post  = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+
+        if (isLiked) {
+            post.likes.delete(userId)
+        } else {
+            post.likes.set(userId, true);
+        }
+
+        const updatePost = await Post.findByIdAndUpdate(
+            id,
+            { likes: post.likes },
+            { new: true }
+        );
+        
+        res.status(201).json();
+    } catch (err) {
+        res.status(404).json({ messega: err.messega });
+    }
+}
