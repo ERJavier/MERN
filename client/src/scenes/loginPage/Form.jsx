4e1d82c1ef5db74a5bdacc066f947714805dfb15
Javier Cepeda <erjavierc@gmail.com>
2023-02-12 14:14:50 -0500

    dropzone doneeeeegit add .git add .git add .

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -54,5 +54,134 @@ const Form = () => {
     const isNonMobile = useMediaQuery("(min-width:600px)");
     const isLogin = pageType === "login";
     const isRegister = pageType === "register";
+
+    const handleFormSubmit = async (values, onSubmitProps) => {
+
+    }
+
+    return (
+        <Formik
+            onSubmit={handleFormSubmit}
+            initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
+            validationSchema={isLogin ? loginSchema : registerSchema}
+        >
+            {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                setFieldValue,
+                resetForm,
+            }) => {
+                <form onSubmit={handleSubmit}>
+                  <Box
+                    display="grid"
+                    gap="30px"
+                    gridTemplateColumns="repeat(4, minmax(o, 1fr))"
+                    sx={{
+                      "& > div": {
+                        gridColumn: isNonMobile ? undefined : "span 4",
+                      },
+                    }}
+                  >
+                    {isRegister && (
+                      <>
+                        <TextField
+                          label="First Name"
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          value={values.firstName}
+                          name="firstName"
+                          error={
+                            Boolean(touched.firstName) &&
+                            Boolean(errors.firstName)
+                          }
+                          helperText={touched.firstName && errors.firstName}
+                          sx={{ gridColumn: "span 2" }}
+                        />
+                        <TextField
+                          label="Last Name"
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          value={values.lastName}
+                          name="lastName"
+                          error={
+                            Boolean(touched.lasttName) &&
+                            Boolean(errors.lasttName)
+                          }
+                          helperText={touched.firstName && errors.firstName}
+                          sx={{ gridColumn: "span 2" }}
+                        />
+                        <TextField
+                          label="Location"
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          value={values.location}
+                          name="location"
+                          error={
+                            Boolean(touched.location) &&
+                            Boolean(errors.location)
+                          }
+                          helperText={touched.location && errors.location}
+                          sx={{ gridColumn: "span 4" }}
+                        />
+                        <TextField
+                          label="Occupation"
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          value={values.occupation}
+                          name="firstName"
+                          error={
+                            Boolean(touched.occupation) &&
+                            Boolean(errors.occupation)
+                          }
+                          helperText={touched.occupation && errors.occupation}
+                          sx={{ gridColumn: "span 4" }}
+                        />
+                        <Box
+                            gridColumn="span 4"
+                            border={`1px solid ${palette.neutral.medium}`}
+                            borderRadius="5px"
+                            p="1rem"
+                        >
+                            <Dropzone
+                                acceptedFiles=".jpg,.jpeg,.png"
+                                multiple={false}
+                                onDrop={(acceptedFiles) => 
+                                    setFieldValue("picture", acceptedFiles[0])
+                                }
+                            >
+                                {({ getRootProps, getInputProps }) => (
+                                    <Box
+                                        {...getRootProps()}
+                                        border={`2px dashed ${palette.primary.main}`}
+                                        p="1rem"
+                                        sx={{ "&:hover": { cursor: "pointer" } }}
+                                    >
+                                        <input {...getInputProps} />
+                                        {!values.picture ? (
+                                            <p>Add Picture Here</p>
+                                        ) : (
+                                            <FlexBetween>
+                                                <Typography>{values.picture.name}</Typography>
+                                                <EditOutlinedIcon />
+                                            </FlexBetween>
+                                        )}
+                                    </Box>
+                                )}
+
+                            </Dropzone>
+
+                        </Box>
+                      </>
+                    )}
+                  </Box>
+                </form>;
+            }}
+
+        </Formik>
+    )
 }
 export default Form
