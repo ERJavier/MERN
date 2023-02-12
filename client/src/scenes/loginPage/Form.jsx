b7b27f71415000804a371e87b4deca97433db6a8
Javier Cepeda <erjavierc@gmail.com>
2023-02-12 13:47:17 -0500

    create the form to register, and make sure they sign in

---
+++ client/src/scenes/loginPage/Form.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import {
+    Box,
+    Button,
+    TextField,
+    useMediaQuery,
+    Typography,
+    useTheme
+} from "@mui/material"
+import EditOutlinedIcon from "@mui/icons-material/EditOffOutlined"
+import { Formik } from "formik";
+import * as yup from "yup"
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setLogin } from "state";
+import Dropzone from "react-dropzone";
+import FlexBetween from "components/FlexBetween";
+
+
+const registerSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+  location: yup.string().required("required"),
+  occupation: yup.string().required("required"),
+  picture: yup.string().required("required"),
+});
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+});
+
+const initialValuesRegister = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  location: "",
+  occupation: "",
+  picture: "",
+};
+const initialValuesLogin = {
+  email: "",
+  password: "",
+};
+
+const Form = () => {
+    const [pageType, setPageType] = useState("login");
+    const { palette } = useTheme();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isNonMobile = useMediaQuery("(min-width:600px)");
+    const isLogin = pageType === "login";
+    const isRegister = pageType === "register";
+}
+export default Form
--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,33 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
+
 const LoginPage = () => {
-    console.log(232323)
-    return <div>loginpage</div>;
-    };
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box
+      width="100%"
+      backgroundColor={theme.palette.alt}
+      p="1rem 6%"
+      textAlign="center"
+    >
+      <Typography fontWeight="bold" fontSize="32px" color="primary">
+        Sociopedia
+      </Typography>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+        >
+            <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }} >
+                Welcome to Sociopedia, the Scoaul Media for Socipaths!
+            </Typography>
+      </Box>
+    </Box>
+  );
+};
 
 export default LoginPage;
