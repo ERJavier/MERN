475942b96cabf8ee58eaa9695a5de57e4117e002
Javier Cepeda <erjavierc@gmail.com>
2023-02-13 13:58:41 -0500

    register and login form

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -55,8 +55,56 @@ const Form = () => {
     const isLogin = pageType === "login";
     const isRegister = pageType === "register";
 
-    const handleFormSubmit = async (values, onSubmitProps) => {
 
+    const register = async (values, onSubmitProps) => {
+        // this allows us to send form info with images
+        const formData = new FormData();
+        for (let value in values) {
+            formData.append(value, values[value])
+        }
+        formData.append('picturePath', values.picture.name);
+
+        const savedUserResponse = await fetch(
+            "http://localhost:3001/auth/register",
+            {
+                method: "POST",
+                body: formData,
+            }
+        );
+        const savedUser = await savedUserResponse.json();
+        onSubmitProps.resetForm();
+
+        if (savedUser) {
+            setPageType("login");
+        }
+    };
+
+    const login = async (values, onSubmitProps) => {
+      const loggedInResponse = await fetch(
+        "http://localhost:3001/auth/login",
+        {
+          method: "POST",
+          header: { "Content-Type": "application/json"},
+          body: JSON.stringify(values),
+        }
+      );
+      const loggedIn = await loggedInResponse.json();
+      onSubmitProps.resetForm();
+      if (loggedIn) {
+        dispatch(
+            setLogin({
+                user: loggedIn.user,
+                token: loggedIn.token,
+            })
+        );
+        navigate("/home");
+      }
+    };
+
+
+    const handleFormSubmit = async (values, onSubmitProps) => {
+        if (isLogin) await login(values, onSubmitProps);
+        if (isRegister) await register(values, onSubmitProps);
     }
 
     return (
@@ -141,42 +189,102 @@ const Form = () => {
                           sx={{ gridColumn: "span 4" }}
                         />
                         <Box
-                            gridColumn="span 4"
-                            border={`1px solid ${palette.neutral.medium}`}
-                            borderRadius="5px"
-                            p="1rem"
+                          gridColumn="span 4"
+                          border={`1px solid ${palette.neutral.medium}`}
+                          borderRadius="5px"
+                          p="1rem"
                         >
-                            <Dropzone
-                                acceptedFiles=".jpg,.jpeg,.png"
-                                multiple={false}
-                                onDrop={(acceptedFiles) => 
-                                    setFieldValue("picture", acceptedFiles[0])
-                                }
-                            >
-                                {({ getRootProps, getInputProps }) => (
-                                    <Box
-                                        {...getRootProps()}
-                                        border={`2px dashed ${palette.primary.main}`}
-                                        p="1rem"
-                                        sx={{ "&:hover": { cursor: "pointer" } }}
-                                    >
-                                        <input {...getInputProps} />
-                                        {!values.picture ? (
-                                            <p>Add Picture Here</p>
-                                        ) : (
-                                            <FlexBetween>
-                                                <Typography>{values.picture.name}</Typography>
-                                                <EditOutlinedIcon />
-                                            </FlexBetween>
-                                        )}
-                                    </Box>
+                          <Dropzone
+                            acceptedFiles=".jpg,.jpeg,.png"
+                            multiple={false}
+                            onDrop={(acceptedFiles) =>
+                              setFieldValue("picture", acceptedFiles[0])
+                            }
+                          >
+                            {({ getRootProps, getInputProps }) => (
+                              <Box
+                                {...getRootProps()}
+                                border={`2px dashed ${palette.primary.main}`}
+                                p="1rem"
+                                sx={{ "&:hover": { cursor: "pointer" } }}
+                              >
+                                <input {...getInputProps} />
+                                {!values.picture ? (
+                                  <p>Add Picture Here</p>
+                                ) : (
+                                  <FlexBetween>
+                                    <Typography>
+                                      {values.picture.name}
+                                    </Typography>
+                                    <EditOutlinedIcon />
+                                  </FlexBetween>
                                 )}
-
-                            </Dropzone>
-
+                              </Box>
+                            )}
+                          </Dropzone>
                         </Box>
                       </>
                     )}
+                    <TextField
+                      label="Email"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.email}
+                      name="email"
+                      error={Boolean(touched.email) && Boolean(errors.email)}
+                      helperText={touched.email && errors.email}
+                      sx={{ gridColumn: "span 4" }}
+                    />
+                    <TextField
+                      label="Password"
+                      type="password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.password}
+                      name="firstName"
+                      error={
+                        Boolean(touched.password) &&
+                        Boolean(errors.password)
+                      }
+                      helperText={touched.password && errors.password}
+                      sx={{ gridColumn: "span 4" }}
+                    />
+                  </Box>
+                  {/* BUTTONS */}
+                  <Box>
+                    <Button
+                        fullWidth
+                        type="submit"
+                        sx={{
+                            m: "2rem 0",
+                            p: "1rem",
+                            backgroundColor: palette.primary.main,
+                            color: palette.background.alt,
+                            "&:hover": { color: palette.primary.main },
+                        }}
+                    >
+                        {isLogin ? "LOGIN": "REGISTER"}
+                    </Button>
+                    <Typography
+                        onClick={() => {
+                            setPageType(isLogin ? "register": "login");
+                            resetForm();
+                        }}
+                        sx={{
+                            textDecoration: "underline",
+                            color: palette.primary.main,
+                            "&:hover": {
+                                cursor: "pointer",
+                                color: palette.primary.light,
+                            },
+                        }}
+                    >
+                        {isLogin 
+                            ? "Don't have an account? Sing up here. "
+                            : "Already have an account? Login here."
+                        }
+
+                    </Typography>
                   </Box>
                 </form>;
             }}
