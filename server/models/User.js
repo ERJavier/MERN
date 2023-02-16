c1888697c99e5c0f79fa65937602fbf959e66cdb
Javier Cepeda <erjavierc@gmail.com>
2023-02-15 19:57:41 -0500

    fixing error 500

--- server/models/User.js
+++ server/models/User.js
@@ -1,43 +1,44 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 50,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
   },
-  lastName: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 50,
-  },
-  email: {
-    type: String,
-    required: true,
-    max: 50,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 5,
-  },
-  picturePath: {
-    type: String,
-    default: "",
-  },
-  friends: {
-    type: Array,
-    default: []
-  },
-  location: String,
-  occupation: String,
-  viewedPorfile: Number,
-  impressions: Number
-},
-    { timestamp: true }
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", UserSchema);
