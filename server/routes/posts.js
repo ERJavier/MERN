ef663b97eb7c2871e2745245db4bdf462e2e9b80
Javier Cepeda <erjavierc@gmail.com>
2023-02-21 20:32:31 -0500

    final commit / app completed

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,17 +1,14 @@
-import express  from "express";
-import { getFeedPosts, getUserPosts, likePosts } from "../controllers/posts.js"
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-//READ 
-
+/* READ */
 router.get("/", verifyToken, getFeedPosts);
-router.get("/:userID/posts", verifyToken, getUserPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
-
-//UPDATE
-
-router.patch("/:id/like", verifyToken, likePosts);
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
 
 export default router;
